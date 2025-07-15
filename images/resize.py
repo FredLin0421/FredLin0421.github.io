3c93c6ac2ef95ccfc2bc2c55531f1d9af1f56bd6
JUI-TE LIN <fredlin@JUI-TEs-MacBook-Pro-1080.local>
2025-07-14 22:55:14 -0400

    new version

--- images/resize.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.imread('laparoscopic.png')
-reimg = cv2.resize(img,(300,300))
-
-cv2.imwrite('laparoscopic_re.png',reimg)
-cv2.imshow("Resized image", reimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
